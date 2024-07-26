bdd97bad33c5fba3336bedfc7a0d82b8ec7bdcc1
Your Name <you@example.com>
2024-07-26 10:45:50 -0700

    Task 4

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+""" MRU Cache module """
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """ MRUCache defines a Most Recently Used caching system """
+    def __init__(self):
+        """ Initialize MRUCache """
+        super().__init__()
+        self.usage_order = []
+
+    def put(self, key, item):
+        """ Add an item in the cache """
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.usage_order.remove(key)
+            self.usage_order.append(key)
+        else:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                mru_key = self.usage_order.pop()
+                del self.cache_data[mru_key]
+                print(f"DISCARD: {mru_key}")
+            self.cache_data[key] = item
+            self.usage_order.append(key)
+
+    def get(self, key):
+        """ Get an item by key """
+        if key is None or key not in self.cache_data:
+            return None
+        self.usage_order.remove(key)
+        self.usage_order.append(key)
+        return self.cache_data[key]
